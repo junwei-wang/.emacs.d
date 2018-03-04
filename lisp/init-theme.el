bbfb42bc70bda93ad9cde9ff49e6ce836ba1b029
Junwei Wang <i.junwei.wang@gmail.com>
2018-03-04 08:30:56 +0100

    update theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,5 +1,13 @@
+(require-package 'ample-theme)
+;; load theme
+(load-theme 'ample t t)
+(load-theme 'ample-flat t t)
+(load-theme 'ample-light t t)
+(enable-theme 'ample-flat)
+
+;; dimmer mode
 (when (maybe-require-package 'dimmer)
-  (setq-default dimmer-fraction 0.1)
+  (setq-default dimmer-fraction 0.5)
   (add-hook 'after-init-hook 'dimmer-mode))
 
 
