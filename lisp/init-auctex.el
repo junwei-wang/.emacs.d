d163d170f29894c4b6349c3bd47be8e0893531e8
Junwei Wang <junwei.wang@cryptoexperts.com>
2018-05-12 11:39:01 +0200

    update

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -18,7 +18,7 @@
       (list 'auto-fill-mode
 	    #'turn-on-flyspell))
 
-(add-hook 'LaTeX-mode-hook (lambda () (set-fill-column 100)))
+(add-hook 'LaTeX-mode-hook (lambda () (set-fill-column 80)))
 
 ;; (add-hook 'LaTeX-mode-hook 'visual-line-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
