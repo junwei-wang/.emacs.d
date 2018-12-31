785f9d683e6b63903ca3db6ae81eb8517e27a5a3
Junwei Wang <i.junwei.wang@gmail.com>
2018-12-31 11:17:49 +0100

    Update custome

--- custom.el
+++ custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (nimbus-theme ample-theme smex sage-shell-mode markdown-mode json-mode diminish counsel ivy-historian ivy ibuffer-vc magit diff-hl guide-key comment-dwim-2 helm-projectile helm-gtags helm yasnippet ws-butler company clean-aindent-mode counsel-projectile volatile-highlights dtrt-indent anzu undo-tree use-package zygospore auto-complete auctex-latexmk auctex fullframe seq dimmer org org-preview-html pdf-toolss pdf-tools))))
+    (nimbus-theme ample-theme smex sage-shell-mode markdown-mode json-mode org-pomodoro writeroom-mode org-cliplink grab-mac-link diminish counsel ivy-historian ivy ibuffer-vc magit diff-hl guide-key comment-dwim-2 helm-projectile helm-gtags helm yasnippet ws-butler company clean-aindent-mode counsel-projectile volatile-highlights dtrt-indent anzu undo-tree use-package zygospore auto-complete auctex-latexmk auctex fullframe seq dimmer org org-preview-html pdf-toolss pdf-tools)))
+ '(safe-local-variable-values
+   (quote
+    ((TeX-command-extra-options . " -interaction=batchmode -shell-escape --synctex=1")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
