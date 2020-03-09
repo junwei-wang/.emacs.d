b794bfe4d20347b55ebae72b23e8aedf7f756ee4
Junwei Wang <junwei.wang@cryptoexperts.com>
2020-03-09 10:27:55 +0100

    Add automode

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,3 +1,8 @@
+(defun add-auto-mode (mode &rest patterns)
+  "Add entries to `auto-mode-alist' to use `MODE' for all given file `PATTERNS'."
+  (dolist (pattern patterns)
+    (add-to-list 'auto-mode-alist (cons pattern mode))))
+
 (if (fboundp 'with-eval-after-load)
     (defalias 'after-load 'with-eval-after-load)
   (defmacro after-load (feature &rest body)
